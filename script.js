0b287caeb5416fcf80cdc1fb5050cd798a55e6b9
Dalibor <127254380+daliborbuca@users.noreply.github.com>
2024-07-07 12:09:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelector('section');
-let navlinks = document.querySelector('header nava');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    let top = window.scrollY;
     sections.forEach(sec => {
-        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
+            navLinks.forEach(link => {
+                link.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         }
     });
-}
-
-
-
-
-
+};
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active'); 
-}
+};
+
+// Responsive handling
+window.onresize = () => {
+    if (window.innerWidth > 768) {
+        navbar.classList.remove('active');
+        menuIcon.classList.remove('bx-x');
+    }
+};
